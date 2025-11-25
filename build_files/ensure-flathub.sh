4ca61676841b60d392f76b8417f0d035ecdda835
Skyler Mayfield <skyler544@gmail.com>
2025-11-25 15:21:33 +0100

    Replace fedora flatpaks in a timer

--- build_files/ensure-flathub.sh
+++ build_files/ensure-flathub.sh
@@ -10,23 +10,22 @@ curl --retry 3 -Lo \
      "https://dl.flathub.org/repo/flathub.flatpakrepo"
 
 
-# OVERWRITE SERVICE
+# REPLACE FEDORA FLATPAK REMOTES
 # ----------------------------------------------------
-rm -f /usr/lib/systemd/system/flatpak-add-flathub-repos.service || true
-cat > /usr/lib/systemd/system/flatpak-add-flathub-repos.service <<'EOF'
+rm -f /usr/lib/systemd/system/flatpak-add-fedora-repos.service || true
+cat > /usr/lib/systemd/system/replace-fedora-flatpak-remotes.service <<'EOF'
 [Unit]
-Description=Add Flathub flatpak repositories and replace all Fedora flatpaks.
+Description=Add Flathub flatpak remotes and remove Fedora flatpak remotes.
 ConditionPathExists=!/var/lib/flatpak/.minus-one-flatpak-initialized
 Before=flatpak-system-helper.service
 
 [Service]
 Type=oneshot
 RemainAfterExit=yes
-ExecStart=/usr/bin/flatpak remote-delete --force fedora
-ExecStart=/usr/bin/flatpak remote-delete --force fedora-testing
+ExecStart=-/usr/bin/flatpak remote-delete --force fedora
+ExecStart=-/usr/bin/flatpak remote-delete --force fedora-testing
 ExecStart=/usr/bin/flatpak remote-add --if-not-exists flathub /etc/flatpak/remotes.d/flathub.flatpakrepo
 ExecStart=/usr/bin/flatpak remote-modify --enable flathub
-ExecStart=-/bin/bash -c '/usr/bin/flatpak install --reinstall --assumeyes flathub $(/usr/bin/flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1)'
 ExecStartPost=/usr/bin/touch /var/lib/flatpak/.minus-one-flatpak-initialized
 
 [Install]
@@ -34,6 +33,34 @@ WantedBy=multi-user.target
 EOF
 
 
-# START SERVICE
+# REPLACE FEDORA FLATPAKS
 # ----------------------------------------------------
-systemctl enable flatpak-add-flathub-repos.service
+cat > /usr/lib/systemd/system/replace-installed-fedora-flatpaks.service <<'EOF'
+[Unit]
+Description=Replace all installed Fedora flatpaks with Flathub versions.
+Wants=network-online.target
+After=network-online.target replace-fedora-flatpak-remotes.service
+ConditionPathExists=!/var/lib/flatpak/.minus-one-flatpaks-replaced
+
+[Service]
+Type=oneshot
+ExecStart=-/bin/bash -c '/usr/bin/flatpak install --reinstall --assumeyes flathub $(/usr/bin/flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1)'
+ExecStartPost=/usr/bin/touch /var/lib/flatpak/.minus-one-flatpaks-replaced
+EOF
+cat > /usr/lib/systemd/system/replace-installed-fedora-flatpaks.timer <<'EOF'
+[Unit]
+Description=Run replace-fedora-flatpaks shortly after boot
+
+[Timer]
+OnBootSec=2min
+Unit=replace-installed-fedora-flatpaks.service
+Persistent=yes
+
+[Install]
+WantedBy=timers.target
+EOF
+
+# ENABLE SERVICES
+# ----------------------------------------------------
+systemctl enable replace-fedora-flatpak-remotes.service
+systemctl enable replace-installed-fedora-flatpaks.timer
