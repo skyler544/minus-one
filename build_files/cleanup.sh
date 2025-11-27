8b2feb172370fd7e703b860b0bb57b392ae66475
Skyler Mayfield <skyler544@gmail.com>
2025-11-27 17:00:34 +0100

    Adapt stuff from ublue's cleanup script

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -4,7 +4,16 @@ set -ouex pipefail
 
 # CLEANUP
 # ----------------------------------------------------
+# Remove all bootc lint violations
 rm -rf /tmp/* || true
-rm -f /var/log/dnf*.log || true
+rm -rf /var/log/ || true
 rm -rf /var/lib/dnf || true
 rm -rf /var/lib/alternatives || true
+rm -rf /usr/etc || true
+rm -rf /boot && mkdir /boot
+# Remove everything from /var/ other than caches
+find /var/* -maxdepth 0 -type d \! -name cache \! -name log -exec rm -rf {} \;
+find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 -exec rm -rf {} \;
+# Ensure tmp dir exists
+mkdir -p /var/tmp
+chmod -R 1777 /var/tmp
