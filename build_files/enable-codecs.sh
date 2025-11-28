f3fb24affdb20c34353a55d2717d98f9d1a6b496
Skyler Mayfield <skyler544@gmail.com>
2025-11-28 10:04:20 +0100

    Add all codecs from @multimedia group manually

--- build_files/enable-codecs.sh
+++ build_files/enable-codecs.sh
@@ -15,8 +15,33 @@ $DNF install \
 $DNF swap ffmpeg-free ffmpeg --allowerasing
 
 
+# UPDATE CODECS
+# ----------------------------------------------------
+CODEC_PACKAGES=(
+    fdk-aac
+    gstreamer1-libav
+    gstreamer1-plugin-dav1d
+    gstreamer1-plugin-openh264
+    gstreamer1-plugins-bad-freeworld
+    gstreamer1-plugins-ugly
+    lame
+    libaom
+    libavcodec-freeworld
+    libdvdcss
+    libopus
+    libtheora
+    libvorbis
+    libvpx
+    libwebp
+    openh264
+    x264
+    x265
+)
+$DNF install "${CODEC_PACKAGES[@]}"
+
+
 # UPDATE DRIVERS
 # ----------------------------------------------------
-$DNF install intel-media-driver
+$DNF install intel-media-driver libva-intel-media-driver
 $DNF swap mesa-va-drivers mesa-va-drivers-freeworld
 $DNF swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
