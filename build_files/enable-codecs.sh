31739c0a18a240721a13a294ee9138cb5d633736
Skyler Mayfield <skyler544@gmail.com>
2025-11-28 10:39:03 +0100

    Use group argument

--- build_files/enable-codecs.sh
+++ build_files/enable-codecs.sh
@@ -17,27 +17,9 @@ $DNF swap ffmpeg-free ffmpeg --allowerasing
 
 # UPDATE CODECS
 # ----------------------------------------------------
-CODEC_PACKAGES=(
-    fdk-aac
-    gstreamer1-libav
-    gstreamer1-plugin-dav1d
-    gstreamer1-plugin-openh264
-    gstreamer1-plugins-bad-freeworld
-    gstreamer1-plugins-ugly
-    lame
-    libaom
-    libavcodec-freeworld
-    libdvdcss
-    libopus
-    libtheora
-    libvorbis
-    libvpx
-    libwebp
-    openh264
-    x264
-    x265
-)
-$DNF install "${CODEC_PACKAGES[@]}"
+$DNF group install multimedia \
+     --setopt="install_weak_deps=False" \
+     --exclude=PackageKit-gstreamer-plugin
 
 
 # UPDATE DRIVERS
