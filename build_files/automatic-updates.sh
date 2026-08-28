b9fd40b20e044f707659eda3c573bd852857f376
Skyler Mayfield <skyler544@gmail.com>
2026-08-28 19:59:32 +0200

    Fix update-flatpaks unit file to work around static deltas problem

--- build_files/automatic-updates.sh
+++ build_files/automatic-updates.sh
@@ -32,7 +32,7 @@ TimeoutStartSec=2h
 ExecStartPre=/usr/sbin/bash -c 'for i in {1..60}; do getent hosts ghcr.io >/dev/null 2>&1 && exit 0 || sleep 1; done; exit 1'
 ExecStart=/usr/sbin/flatpak repair
 ExecStart=/usr/sbin/flatpak uninstall --assumeyes --unused --noninteractive
-ExecStart=/usr/sbin/flatpak update --assumeyes --noninteractive
+ExecStart=/usr/sbin/flatpak update --assumeyes --noninteractive --no-static-deltas
 
 [Install]
 WantedBy=default.target
